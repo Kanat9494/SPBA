67bc482d901d5046b3818d7198ec0f9b87649630
kana2022 <kkanat94@gmail.com>
2023-06-02 17:59:23 +0600

    02062023

--- SPBA/MauiProgram.cs
+++ SPBA/MauiProgram.cs
@@ -22,6 +22,16 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
+		Microsoft.Maui.Handlers.SearchBarHandler.Mapper.AppendToMapping("MyCustomizationSearchBar", (handler, view) =>
+		{
+#if ANDROID
+			Android.Widget.LinearLayout linearLayout = handler.PlatformView.GetChildAt(0) as Android.Widget.LinearLayout;
+			linearLayout = linearLayout.GetChildAt(2) as Android.Widget.LinearLayout;
+            linearLayout = linearLayout.GetChildAt(1) as Android.Widget.LinearLayout;
+            linearLayout.Background = null;
+#endif
+        });
+
 		return builder.Build();
 	}
 }
